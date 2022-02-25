5d6fa35b4f46d8513b9e9bc9ed02330c1f756669
Jakpat Vanichjakvong <Vanichjakvong.J@etoncollege.org.uk>
2022-02-25 22:54:37 +0000

    Initial Commit

---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by Vanichjakvong, Jakpat (IRG) on 25/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
